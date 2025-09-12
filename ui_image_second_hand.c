788c060bc6f5c8cf5db4238197156ab146302726
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2025-09-12 15:59:21 -0400

    TRUE_COLOR_CHROMA

--- ui_image_second_hand.c
+++ ui_image_second_hand.c
@@ -568,7 +568,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_IMG_SECON
 };
 
 const lv_img_dsc_t img_second_hand = {
-  .header.cf = LV_IMG_CF_TRUE_COLOR,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_CHROMA_KEYED,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 8,
